dd858ff302ae421932d976a5084cbfaf11801b3b
Allen Bai <abai@redhat.com>
2019-07-12 14:09:54 -0400

    ls /sysroot/ostree/deploy/fedora-coreos/deploy

--- dracut/30ignition/ignition-apply-kargs.sh
+++ dracut/30ignition/ignition-apply-kargs.sh
@@ -65,7 +65,7 @@ reboot_if_kargs_dir_exists() {
     if [ -d /sysroot/etc/ostree/kargs.d ]; then
         local kargs=$(cat /sysroot/etc/ostree/kargs.d/testfile)
         # ostree admin --sysroot=/sysroot --os=fcos deploy ${REFSPEC}
-        echo "ls /sysroot/ostree/deploy/fedora-coreos/ $(ls /sysroot/ostree/deploy/fedora-coreos)"
+        echo "ls /sysroot/ostree/deploy/fedora-coreos/deploy: $(ls /sysroot/ostree/deploy/fedora-coreos/deploy)"
         ostree admin -v --sysroot=/sysroot instutil set-kargs --replace ${kargs}
         exec systemctl reboot
     fi
